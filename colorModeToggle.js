cc1ce27537d76bd24e8c85355f2d1fc1a76e357a
preandpost <154316762+preandpost@users.noreply.github.com>
2024-01-04 10:49:50 +0100

    Update colorModeToggle.js

--- colorModeToggle.js
+++ colorModeToggle.js
@@ -1,13 +1,65 @@
+/**
+ * Dark Mode Toggle 1.0.1
+ * Copyright 2023 Timothy Ricks
+ * Released under the MIT License
+ * Released on: November 28, 2023
+ */
+
 function colorModeToggle() {
+  function attr(defaultVal, attrVal) {
+    const defaultValType = typeof defaultVal;
+    if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+    if (attrVal === "true" && defaultValType === "boolean") return true;
+    if (attrVal === "false" && defaultValType === "boolean") return false;
+    if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+    if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+    return defaultVal;
+  }
+
   const htmlElement = document.documentElement;
+  const computed = getComputedStyle(htmlElement);
   let toggleEl;
+  let modeTextEl; // Added reference to mode text element
+  let togglePressed = "false";
+
+  const scriptTag = document.querySelector("[tr-color-vars]");
+  if (!scriptTag) {
+    console.warn("Script tag with tr-color-vars attribute not found");
+    return;
+  }
+
+  let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
+  let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
+
+  const cssVariables = scriptTag.getAttribute("tr-color-vars");
+  if (!cssVariables.length) {
+    console.warn("Value of tr-color-vars attribute not found");
+    return;
+  }
+
+  let lightColors = {};
+  let darkColors = {};
+  cssVariables.split(",").forEach(function (item) {
+    let lightValue = computed.getPropertyValue(`--color--${item}`);
+    let darkValue = computed.getPropertyValue(`--dark--${item}`);
+    if (lightValue.length) {
+      if (!darkValue.length) darkValue = lightValue;
+      lightColors[`--color--${item}`] = lightValue;
+      darkColors[`--color--${item}`] = darkValue;
+    }
+  });
+
+  if (!Object.keys(lightColors).length) {
+    console.warn("No variables found matching tr-color-vars attribute value");
+    return;
+  }
 
   function setColors(colorObject, animate) {
     if (typeof gsap !== "undefined" && animate) {
       gsap.to(htmlElement, {
         ...colorObject,
-        duration: 0.5,
-        ease: "power1.out",
+        duration: colorModeDuration,
+        ease: colorModeEase
       });
     } else {
       Object.keys(colorObject).forEach(function (key) {
@@ -17,58 +69,29 @@ function colorModeToggle() {
   }
 
   function goDark(dark, animate) {
-    const scriptTag = document.querySelector("[tr-color-vars]");
-    if (!scriptTag) {
-      console.warn("Script tag with tr-color-vars attribute not found");
-      return;
-    }
-
-    const cssVariables = scriptTag.getAttribute("tr-color-vars");
-    if (!cssVariables.length) {
-      console.warn("Value of tr-color-vars attribute not found");
-      return;
-    }
-
-    let lightColors = {};
-    let darkColors = {};
-    cssVariables.split(",").forEach(function (item) {
-      let lightValue = getComputedStyle(htmlElement).getPropertyValue(`--color--${item}`).trim();
-      let darkValue = getComputedStyle(htmlElement).getPropertyValue(`--dark--${item}`).trim();
-      if (lightValue.length) {
-        if (!darkValue.length) darkValue = lightValue;
-        lightColors[`--color--${item}`] = lightValue;
-        darkColors[`--color--${item}`] = darkValue;
-      }
-    });
-
-    if (!Object.keys(lightColors).length) {
-      console.warn("No variables found matching tr-color-vars attribute value");
-      return;
-    }
-
     if (dark) {
       localStorage.setItem("dark-mode", "true");
       htmlElement.classList.add("dark-mode");
       setColors(darkColors, animate);
+      togglePressed = "true";
     } else {
       localStorage.setItem("dark-mode", "false");
       htmlElement.classList.remove("dark-mode");
       setColors(lightColors, animate);
+      togglePressed = "false";
     }
-
+    if (typeof toggleEl !== "undefined") {
+      toggleEl.forEach(function (element) {
+        element.setAttribute("aria-pressed", togglePressed);
+      });
+    }
     updateModeText(); // Update text after mode change
     updateButtonColors(); // Update button colors after mode change
   }
 
   function updateModeText() {
-    const modeText = document.getElementById("modeText");
-    if (!modeText) {
-      console.warn("Mode text element not found");
-      return;
-    }
-
     const darkClass = htmlElement.classList.contains("dark-mode");
-    modeText.textContent = darkClass ? "Light mode" : "Dark mode";
+    modeTextEl.textContent = darkClass ? "Light mode" : "Dark mode"; // Update text content based on mode
   }
 
   function updateButtonColors() {
@@ -96,9 +119,15 @@ function colorModeToggle() {
   }
 
   window.addEventListener("DOMContentLoaded", (event) => {
+    toggleEl = document.querySelectorAll("[tr-color-toggle]");
+    modeTextEl = document.getElementById("modeText"); // Get reference to the mode text element
+    toggleEl.forEach(function (element) {
+      element.setAttribute("aria-label", "View Dark Mode");
+      element.setAttribute("role", "button");
+      element.setAttribute("aria-pressed", togglePressed);
+    });
     updateModeText(); // Set initial text
     updateButtonColors(); // Set initial button colors
-    toggleEl = document.querySelectorAll("[tr-color-toggle]");
     toggleEl.forEach(function (element) {
       element.addEventListener("click", function () {
         let darkClass = htmlElement.classList.contains("dark-mode");
