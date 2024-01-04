687ce2c0949b915e759d83f58d9fbd5468115760
preandpost <154316762+preandpost@users.noreply.github.com>
2024-01-04 10:19:25 +0100

    Add files via upload

---
+++ colorModeToggle.js
@@ -0,0 +1,109 @@
+function colorModeToggle() {
+  function attr(defaultVal, attrVal) {
+    const defaultValType = typeof defaultVal;
+    if (typeof attrVal !== "string" || attrVal.trim() === "") return defaultVal;
+    if (attrVal === "true" && defaultValType === "boolean") return true;
+    if (attrVal === "false" && defaultValType === "boolean") return false;
+    if (isNaN(attrVal) && defaultValType === "string") return attrVal;
+    if (!isNaN(attrVal) && defaultValType === "number") return +attrVal;
+    return defaultVal;
+  }
+
+  const htmlElement = document.documentElement;
+  const computed = getComputedStyle(htmlElement);
+  let toggleEl;
+
+  const scriptTag = document.querySelector("[tr-color-vars]");
+  if (!scriptTag) {
+    console.warn("Script tag with tr-color-vars attribute not found");
+    return;
+  }
+
+  let colorModeDuration = attr(0.5, scriptTag.getAttribute("duration"));
+  let colorModeEase = attr("power1.out", scriptTag.getAttribute("ease"));
+
+  const cssVariables = scriptTag.getAttribute("tr-color-vars");
+  if (!cssVariables.length) {
+    console.warn("Value of tr-color-vars attribute not found");
+    return;
+  }
+
+  let lightColors = {};
+  let darkColors = {};
+  cssVariables.split(",").forEach(function (item) {
+    let lightValue = computed.getPropertyValue(`--color--${item}`);
+    let darkValue = computed.getPropertyValue(`--dark--${item}`);
+    if (lightValue.length) {
+      if (!darkValue.length) darkValue = lightValue;
+      lightColors[`--color--${item}`] = lightValue;
+      darkColors[`--color--${item}`] = darkValue;
+    }
+  });
+
+  if (!Object.keys(lightColors).length) {
+    console.warn("No variables found matching tr-color-vars attribute value");
+    return;
+  }
+
+  function setColors(colorObject, animate) {
+    if (typeof gsap !== "undefined" && animate) {
+      gsap.to(htmlElement, {
+        ...colorObject,
+        duration: colorModeDuration,
+        ease: colorModeEase,
+      });
+    } else {
+      Object.keys(colorObject).forEach(function (key) {
+        htmlElement.style.setProperty(key, colorObject[key]);
+      });
+    }
+  }
+
+  function goDark(dark, animate) {
+    if (dark) {
+      localStorage.setItem("dark-mode", "true");
+      htmlElement.classList.add("dark-mode");
+      setColors(darkColors, animate);
+    } else {
+      localStorage.setItem("dark-mode", "false");
+      htmlElement.classList.remove("dark-mode");
+      setColors(lightColors, animate);
+    }
+
+    updateModeText(); // Update text after mode change
+  }
+
+  function updateModeText() {
+    const modeText = document.getElementById("modeText");
+    if (!modeText) {
+      console.warn("Mode text element not found");
+      return;
+    }
+
+    const darkClass = htmlElement.classList.contains("dark-mode");
+    modeText.textContent = darkClass ? "Light mode" : "Dark mode";
+  }
+
+  function checkPreference(e) {
+    goDark(e.matches, false);
+  }
+
+  const colorPreference = window.matchMedia("(prefers-color-scheme: dark)");
+  colorPreference.addEventListener("change", (e) => {
+    checkPreference(e);
+  });
+
+  let storagePreference = localStorage.getItem("dark-mode");
+  if (storagePreference !== null) {
+    storagePreference === "true" ? goDark(true, false) : goDark(false, false);
+  } else {
+    checkPreference(colorPreference);
+  }
+
+  window.addEventListener("DOMContentLoaded", (event) => {
+    updateModeText(); // Set initial text
+    toggleEl = document.querySelectorAll("[tr-color-toggle]");
+  });
+}
+
+colorModeToggle();
